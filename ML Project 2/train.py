82881f486e60e42b069ae5c28888da346f49c581
Leovaldez42 <ghsharma_b18@et.vjti.ac.in>
2020-07-24 17:19:39 +0530

    added twitter sentiment analysis

---
+++ ML Project 2/train.py
@@ -0,0 +1,33 @@
+import json
+from nltk_util import tokenize, stem, bag_of_words
+import numpy as np
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+
+ignore_words = ['?', '!', '.', ',']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+X_train = []
+Y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tag.index(tag)
+    Y_train.append(label)
